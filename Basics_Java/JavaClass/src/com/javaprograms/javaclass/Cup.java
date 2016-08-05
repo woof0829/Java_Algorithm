064e474a9d857bc3838ecb5497e6ad23dda2fade
woof <woof0829@gmail.com>
2016-08-05 21:01:50 +0100

    add java basics knowledge

---
+++ Basics_Java/JavaClass/src/com/javaprograms/javaclass/Cup.java
@@ -0,0 +1,17 @@
+package com.javaprograms.javaclass;
+
+/**
+ * Created by Woof on 2016/6/24 0024.
+ */
+public class Cup {
+    //实例变量 instance variables
+    String colour;
+    int capacity;
+
+    //Behaviour 功能functions
+
+    public void shatter(){
+        System.out.println("shatterrrrr!");
+    }
+
+}
---
+++ Basics_Java/JavaClass/src/com/javaprograms/javaclass/javaclasses.java
@@ -0,0 +1,11 @@
+package com.javaprograms.javaclass;
+
+public class javaclasses {
+    /**
+     *@ java class
+     */
+    public static void main(String[] args) {
+        Cup cup1=new Cup();
+        cup1.shatter();
+    }
+}
