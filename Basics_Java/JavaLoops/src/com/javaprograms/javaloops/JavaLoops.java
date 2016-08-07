390e17bc4e1d46231d086316cdf755194bef8fca
woof <woof0829@gmail.com>
2016-08-07 23:32:40 +0100

    add java loops

---
+++ Basics_Java/JavaLoops/src/com/javaprograms/javaloops/JavaLoops.java
@@ -0,0 +1,22 @@
+package com.javaprograms.javaloops;
+
+public class JavaLoops {
+    /**
+     *
+     * @ for loop
+     */
+    public static void main(String[] args) {
+        for (int i=0;i<=10;i++) {
+            System.out.println("Hello World!"+i);
+        }
+    /**
+     * while loop
+     */
+        int value=10;
+        while (value<20){
+            System.out.println(value+" smaller than 20");
+            value+=1;
+
+        }
+    }
+}
